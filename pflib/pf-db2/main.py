8dcf5c37460265fe0cb133c0e29cc2d6ef3f7442
Tim <_>
2020-10-06 23:43:53 +0200

    0

--- pflib/pf-db2/main.py
+++ pflib/pf-db2/main.py
@@ -1,36 +1,35 @@
+# TODO: FIGURE OUT HOW TO USE THE GITHUB SECRETS FOR LIKE PASSWORDS N SHIT
 import pymysql
 import paramiko
 import pandas as pd
 from paramiko import SSHClient
 from sshtunnel import SSHTunnelForwarder
 from os.path import expanduser
+from configparser import ConfigParser
 
+config = ConfigParser()
+print(config.read("x.ini"))
 home = expanduser('~')
-# mypkey = paramiko.RSAKey.from_private_key_file(home + pkeyfilepath)
-# if you want to use ssh password use - ssh_password='your ssh password', bellow
+sql_hostname = config["SSH_tunneled_MySQL"]["sql_hostname"]
+sql_username = config["SSH_tunneled_MySQL"]["sql_username"]
+sql_password = config["SSH_tunneled_MySQL"]["sql_password"]
+sql_main_database = config["SSH_tunneled_MySQL"]["sql_main_database"]
+sql_port = int(config["SSH_tunneled_MySQL"]["sql_port"])
+ssh_host = config["SSH_tunneled_MySQL"]["ssh_host"]
+ssh_user = config["SSH_tunneled_MySQL"]["ssh_user"]
+ssh_port = int(config["SSH_tunneled_MySQL"]["ssh_port"])
+ssh_password = config["SSH_tunneled_MySQL"]["ssh_password"]
+sql_ip = config["SSH_tunneled_MySQL"]["sql_ip"]
 
-sql_hostname = ''
-sql_username = ''
-sql_password = ''
-sql_main_database = 'pfdb1'
-sql_port = 3306
-ssh_host = ''
-ssh_user = ''
-ssh_port = 22
-ssh_password = ''
-sql_ip = '127.0.0.1'
+
 
 with SSHTunnelForwarder(
 		(ssh_host, ssh_port),
 		ssh_username=ssh_user,
-		ssh_pkey=ssh_password,
+		ssh_password=ssh_password,
 		remote_bind_address=(sql_hostname, sql_port)) as tunnel:
 	conn = pymysql.connect(host='127.0.0.1', user=sql_username, passwd=sql_password, db=sql_main_database, port=tunnel.local_bind_port)
-	query = '''SELECT VERSION();'''
+	query = '''SELECT * from dailyASN;'''
 	data = pd.read_sql_query(query, conn)
+	print(data)
 	conn.close()
-
-# TODO: DBeaver
-# TODO: MQTT EXPLORER
-# TODO: MAKE DUMMYGUIDE FOR HOW TO DO THE SQL THING
-# TODO: FIGURE OUT HOW TO USE THE GITHUB SECRETS FOR LIKE PASSWORDS N SHIT
